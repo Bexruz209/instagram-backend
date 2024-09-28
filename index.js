eeb5af26e56e92061f010550e869d4df2a6ca3a2
Bexruz209 <b7079921@gmail.com>
2024-09-28 06:55:15 +0500

    instgarm bacend

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
 const port = process.env.PORT || 1080;
 
-
 server.use(middlewares);
 server.use(router);
-
 
 server.listen(port);
